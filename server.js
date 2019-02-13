7e44cb66df61450645c5347c24668658809dec5b
Dan Halperin <dee.aye.en@gmail.com>
2019-02-13 20:53:02 +0200

    amazing progress

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 var fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 var bodyParser = require('body-parser');
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+  provider: 'google',
+	apiKey: "AIzaSyDXY_aO0xDGZX4BSOkw8w88wLpp0Q7HTIQ"
+};
+
+var geocoder = NodeGeocoder(options);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -12,10 +20,10 @@ app.listen(process.env.PORT || 3001, () => {
 	console.log('listening on 3001')
 });
 
-let db = new sqlite3.Database('jerusalemGEO.db');
+let db = new sqlite3.Database('jerusalem.db');
 
 app.get('/', (req, res) => {
-  let sql = 'SELECT * FROM idk';
+  let sql = 'SELECT * FROM main';
   db.all(sql, [], (err, rows) => {
   if (err) {
     throw err;
@@ -29,25 +37,31 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post('/search', (req, res) => {
+app.post('/search', (req, ress) => {
   console.log(req.body)
   var reqreq = req.body
-  let latcalcup = parseInt(reqreq.x) + parseInt(reqreq.distance)
-  let latcalcdown = parseInt(reqreq.x) - parseInt(reqreq.distance)
-  let loncalcup = parseInt(reqreq.y) + parseInt(reqreq.distance)
-  let loncalcdown = parseInt(reqreq.y) - parseInt(reqreq.distance)
-  let calculated = ' WHERE X < ' + latcalcup.toString() + ' AND X > ' + latcalcdown.toString() + ' AND Y < ' + loncalcup.toString() + ' AND Y > ' + loncalcdown.toString()
-  let sql = 'SELECT * FROM idk' + calculated;
-  console.log(sql)
-  db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  posts = []
-  rows.forEach((row) => {
-    posts.push(row);
-  });
-  console.log(posts)
-  res.render('index.ejs', {posts})
-  });
+	geocoder.geocode(reqreq.address, function(err, res) {
+	  console.log(res[0].latitude);
+		console.log(res[0].longitude);
+		reqreq.x = res[0].latitude;
+		reqreq.y = res[0].longitude;
+		let latcalcup = parseFloat(reqreq.x) + (parseFloat(reqreq.distance) / 111)
+	  let latcalcdown = parseFloat(reqreq.x) - (parseFloat(reqreq.distance) / 111)
+	  let loncalcup = parseFloat(reqreq.y) + (parseFloat(reqreq.distance) / 95)
+	  let loncalcdown = parseFloat(reqreq.y) - (parseFloat(reqreq.distance) / 95)
+	  let calculated = ' WHERE X < ' + latcalcup.toString() + ' AND X > ' + latcalcdown.toString() + ' AND Y < ' + loncalcup.toString() + ' AND Y > ' + loncalcdown.toString()
+	  let sql = 'SELECT * FROM main' + calculated;
+	  console.log(sql)
+	  db.all(sql, [], (err, rows) => {
+	  if (err) {
+	    throw err;
+	  }
+	  posts = []
+	  rows.forEach((row) => {
+	    posts.push(row);
+	  });
+	  console.log(posts)
+	  ress.render('index.ejs', {posts})
+	});
+	});
 });
